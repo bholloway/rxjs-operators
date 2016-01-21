e6097928f74a512a184389fc8ad92ab733441695
benholloway <ben.holloway@3plearning.com>
2016-01-21 15:54:11 +1100

    behavior implemented and passing unit tests

--- operator/behavior.js
+++ operator/behavior.js
@@ -1,101 +1,96 @@
 'use strict';
 
-var Observable = require('rxjs').Observable;
+var Rx                 = require('rxjs'),
+    multicast          = require('rxjs/operator/multicast').multicast,
+    RefCountObservable = (new Rx.ConnectableObservable()).refCount().constructor;
 
-var toObservable = require('./to-observable');
+var subclassWith = require('../utility/subclass-with');
 
 /**
  * Represents a value that changes over time. Observers can subscribe to the subject to receive the last (or initial)
  * value and all subsequent notifications, unless or until the source Observable is complete.
  *
+ * Exposes a `clear()` method that will re-instate the `initialValue`.
+ *
+ * Exposes an `isValid` flag which negates any time the current value is the `initialValue` (by strict equality).
+ *
  * @this {Observable}
- * @param {function():*|*} [initialValue] Optional value to use when invalid or factory thereof (default `undefined`)
- * @param {Scheduler} [scheduler] Optional scheduler for internal use
- * @returns {Observable} An observable with additional `clear()` method and `isValid:boolean` field
+ * @param {*} [initialValue] Optional value to use when invalid (default `undefined`)
+ * @returns {BehaviorObservable} A RefCountObservable with additional `clear()` method and `isValid:boolean` fields
  */
-function behavior(initialValue, scheduler) {
+function behavior(initialValue) {
   /* jshint validthis:true */
-  var currentValue,
-      isAssigned  = false,
-      isDisposed  = false,
-      upstreamObs = this;
 
-  // shared by all subscribers
-  var sourceObs = upstreamObs
-    .do(store, undefined, dispose);
+  // create a sub-class of RefCountObservable
+  //  infer the RefCountObservable class definition by one of its instances
+  var BehaviorObservable = subclassWith({
+    isValid   : {get: getIsValid},
+    value     : {get: getValue},
+    clear     : clear,
+    lift      : lift
+  }, RefCountObservable, constructor);
 
-  var clearStim,
-      clearObs = Observable.create(function (instance) {
-        if (isDisposed) {
-          instance.complete();
-        }
-        else {
-          clearStim = instance;
-        }
-      }, scheduler);
+  return new BehaviorObservable(this, initialValue);
 
-  var sharedObs = Observable.merge(sourceObs, clearObs, scheduler);
-
-  // ensure that new subscribers are notified COMPLETE if the instance is disposed
-  var resultObs = Observable.defer(function () {
-    return isDisposed ?
-      Observable.empty() :
-      Observable.merge(Observable.of(getStartValue()), sharedObs, scheduler);
-  });
-
-  // ensure the result is the correct type
-  var castResultObs = toObservable.call(resultObs, upstreamObs.constructor);
-
-  // composition
-  return Object.defineProperties(castResultObs, {
-    clear     : {value: clear},
-    getIsValid: {value: getIsValid},
-    isValid   : {get: getIsValid}
-  });
-
-  function store(value) {
-    isAssigned = true;
-    currentValue = value;
-  }
-
-  function clear() {
-    if (!isDisposed) {
-      isAssigned = false;
-      currentValue = undefined;
-      clearStim.next(getInitialValue());
-    }
-  }
-
-  function dispose() {
-    if (!isDisposed) {
-      isDisposed = true;
-
-      isAssigned = false;
-      currentValue = undefined;
-      if (clearStim) {
-        clearStim.complete();
-      }
-
-      upstreamObs = null;
-      sourceObs = null;
-      clearStim = clearObs = null;
-      sharedObs = null;
-      resultObs = null;
-      castResultObs = null;
-    }
-  }
-
-  function getIsValid() {
-    return !isDisposed && (currentValue !== getInitialValue());
-  }
-
-  function getStartValue() {
-    return isDisposed ? undefined : isAssigned ? currentValue : getInitialValue();
-  }
-
-  function getInitialValue() {
-    return (typeof initialValue === 'function') ? initialValue() : initialValue;
+  function lift(operator) {
+    /* jshint validthis:true */
+    var observable = new Rx.Observable();
+    observable.source = this;
+    observable.operator = operator;
+    return observable;
   }
 }
 
 module.exports = behavior;
+
+/**
+ * Constructor for the BehaviorObservable class
+ */
+function constructor(source, initialValue) {
+  /* jshint validthis:true */
+  var that = this;
+
+  // create the subject
+  var subject = new Rx.BehaviorSubject(initialValue);
+
+  // quietly go to disposed state when the source Observable errors or completes
+  var monitored = source.do(undefined, setDisposed, setDisposed);
+
+  // super()
+  RefCountObservable.call(this, multicast.call(monitored, subject));
+
+  // private members
+  this._initialValue = initialValue;
+  this._subject = subject;
+  this._isDisposed = false;
+
+  function setDisposed() {
+    that._isDisposed = true;
+  }
+}
+
+/**
+ * Flag negates any time the current value on the StimulusObservable instance is the `initialValue` (by strict equality)
+ */
+function getIsValid() {
+  /* jshint validthis:true */
+  return !this._isDisposed && (this._subject.getValue() !== this._initialValue);
+}
+
+/**
+ * The current value of the instance.
+ */
+function getValue() {
+  /* jshint validthis:true */
+  return this._subject.getValue();
+}
+
+/**
+ * Re-instate the `initialValue` on the StimulusObservable instance
+ */
+function clear() {
+  /* jshint validthis:true */
+  if (this.isValid) {
+    this._subject.next(this._initialValue);
+  }
+}
--- operator/behavior.spec.js
+++ operator/behavior.spec.js
@@ -1,5 +1,193 @@
-describe('behavior', function () {
+'use strict';
 
+var VirtualTimeScheduler = require('rxjs/scheduler/VirtualTimeScheduler').VirtualTimeScheduler;
 
+var subclassWith = require('../utility/subclass-with'),
+    stimulus     = require('./stimulus'),
+    behavior     = require('./behavior');
 
+describe('operator.behavior', function () {
+  var BehaviorObservable,
+      observable,
+      upstream,
+      scheduler,
+      output;
+
+  var outputObserver,
+      outputSubscriptions = [];
+
+  beforeAll(function () {
+    BehaviorObservable = subclassWith({
+      behavior: behavior
+    });
+  });
+
+  beforeAll(function () {
+    scheduler = new VirtualTimeScheduler();
+  });
+
+  describe('notify NEXT', function () {
+
+    beforeAll(newInstance);
+
+    beforeAll(function () {
+      upstream.next('missedValue');
+    });
+
+    describe('of first value', describeSubscribers('initialValue', 1));
+
+    describe('of second value', describeSubscribers(1, 2));
+
+    describe('of third value', describeSubscribers(2, 3));
+
+    function describeSubscribers(value0, value1) {
+      return function () {
+
+        beforeAll(function () {
+          subscribeToOutput();
+          scheduler.flush();
+        });
+
+        it('should be a Behavior insofar as it notifies NEXT existing value', function () {
+          expect(outputObserver.next).toHaveBeenCalledWith(value0);
+        });
+
+        it('should expose the existing value', function () {
+          expect(output.value).toBe(value0);
+        });
+
+        it('should notify NEXT with value after upstream NEXT same value', function () {
+          upstream.next(value1);
+          scheduler.flush();
+
+          expect(outputObserver.next).toHaveBeenCalledWith(value1);
+        });
+
+        it('should expose the new value', function () {
+          expect(output.value).toBe(value1);
+        });
+
+        afterAll(unsubscribeToOutput);
+      };
+    }
+  });
+
+  describe('clear()', function () {
+    var value;
+
+    beforeEach(newInstance);
+
+    beforeEach(function () {
+
+      // subscribe so that the instance sees us emit the upstream value
+      subscribeToOutput();
+      scheduler.flush();
+
+      value = Math.random();
+      upstream.next(value);
+      scheduler.flush();
+
+      // unsubscribe so we don't pollute the test
+      unsubscribeToOutput();
+      scheduler.flush();
+    });
+
+    beforeEach(function () {
+      subscribeToOutput();
+      scheduler.flush();
+    });
+
+    it('should reinstate the initial value', function () {
+      expect(output.value).toBe(value);
+
+      output.clear();
+      scheduler.flush();
+
+      expect(output.value).toBe('initialValue');
+    });
+
+    it('should notify NEXT initialValue', function () {
+      expect(outputObserver.next).toHaveBeenCalledWith(value);
+
+      output.clear();
+      scheduler.flush();
+
+      expect(outputObserver.next).toHaveBeenCalledWith('initialValue');
+    });
+  });
+
+  describe('notify COMPLETE', function () {
+
+    beforeEach(newInstance);
+
+    it('should occur when the upstream observable notifies COMPLETE', function () {
+      subscribeToOutput();
+      scheduler.flush();
+
+      expect(outputObserver.complete).not.toHaveBeenCalled();
+
+      upstream.complete();
+      scheduler.flush();
+
+      expect(outputObserver.complete).toHaveBeenCalled();
+    });
+
+    it('should occur when the upstream observable is already COMPLETE', function () {
+      upstream.complete();
+      scheduler.flush();
+
+      subscribeToOutput();
+      scheduler.flush();
+
+      expect(outputObserver.complete).toHaveBeenCalled();
+    });
+
+    afterEach(unsubscribeToOutput);
+  });
+
+  function newInstance() {
+    upstream = stimulus();
+    observable = upstream
+      .let(BehaviorObservable.from);
+    output = observable.behavior('initialValue');
+  }
+
+  function subscribeToOutput(isDebug) {
+    outputObserver = getObserver(isDebug);
+
+    outputSubscriptions.push(output
+      .subscribeOn(scheduler)
+      .subscribe(outputObserver.next, outputObserver.error, outputObserver.complete)
+    );
+  }
+
+  function unsubscribeToOutput() {
+    outputObserver = null;
+    outputSubscriptions.pop()
+      .unsubscribe();
+  }
 });
+
+function getObserver(isDebug) {
+  var observer = {
+    next    : function (value) {
+      if (isDebug) {
+        console.log('NEXT', value);
+      }
+    },
+    error   : function (value) {
+      if (isDebug) {
+        console.log('NEXT', value);
+      }
+    },
+    complete: function () {
+      if (isDebug) {
+        console.log('COMPLETE');
+      }
+    }
+  };
+  spyOn(observer, 'next').and.callThrough();
+  spyOn(observer, 'error').and.callThrough();
+  spyOn(observer, 'complete').and.callThrough();
+  return observer;
+}
